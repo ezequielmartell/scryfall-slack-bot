8e87105ed961f4129a8a56956dd725d208a5a1d5
ezequiel <ezequiel1@thinkmartell.com>
2024-05-15 17:37:20 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,199 @@
+import os
+from slack_bolt import App
+from dotenv import load_dotenv
+import scry
+import ast
+
+load_dotenv()
+
+# Initialize your app with your bot token and signing secret
+app = App(
+    token=os.environ.get("SLACK_BOT_USER_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+)
+
+
+@app.view("")
+def handle_view_submission_events(ack, body, say):
+    ack()
+    # print(body['view']['blocks'][1]['elements'][0]['value'])
+    # card_id = body['view']['blocks'][1]['elements'][0]['value']
+    # print(body)
+    metadata_dict = ast.literal_eval(body['view']['private_metadata'])
+    card_id = metadata_dict['card']
+    channel = metadata_dict['channel']
+    card = scry.card_by_id(card_id)
+    # print(card)
+    card_section = {
+        "type": "image",
+        "title": {
+            "type": "plain_text",
+            "text": card['name'],
+            "emoji": True
+        },
+        "image_url": card['image_uris']['normal'],
+        "alt_text": card['name']
+    }
+    # print(card_section)
+    say(
+        channel=channel,
+        response_type="in_channel",
+        # as_user=False,
+        blocks=[card_section]
+    )
+
+
+@app.action("Random")
+def handle_some_action(ack, body):
+    ack()
+    # print(body)
+    card = scry.random_card()
+    metadata_dict = ast.literal_eval(body['view']['private_metadata'])
+    channel = metadata_dict['channel']
+    res = app.client.views_update(
+        view_id=body["container"]["view_id"],
+        view={
+            "type": "modal",
+            "title": {
+                "type": "plain_text",
+                "text": "Random Card",
+                "emoji": True
+            },
+            "submit": {
+                "type": "plain_text",
+                "text": "Post",
+                "emoji": True
+            },
+            "close": {
+                "type": "plain_text",
+                "text": "Cancel",
+                "emoji": True
+            },
+            "private_metadata": str({"channel": channel,
+                                     "card": card['id']}),
+            "blocks": [
+                {
+                    "type": "image",
+                    "title": {
+                        "type": "plain_text",
+                        "text": card['name'],
+                        "emoji": True
+                    },
+                    "image_url": card['image_uris']['small'],
+                    "alt_text": card['name']
+                },
+                {
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Shuffle",
+                                "emoji": True
+                            },
+                            "value": card['id'],
+                            "action_id": "Random"
+                        }
+                    ]
+                }
+            ]
+        }
+    )
+
+
+@app.command("/scryfall")
+def scryfall(ack, command, body, respond):
+    # Acknowledge command request
+    ack()
+    text = command.get('text')
+    if text:
+        card = scry.search_card(search_term=text)
+        respond(
+            response_type="ephemeral",
+            blocks=card
+        )
+    else:
+        # print(body)
+        card = scry.random_card()
+        # print(f"random: {card}")
+        res = app.client.views_open(
+            trigger_id=body["trigger_id"],
+            view={
+                "type": "modal",
+                "title": {
+                    "type": "plain_text",
+                    "text": "Random Card",
+                    "emoji": True
+                },
+                "submit": {
+                    "type": "plain_text",
+                    "text": "Post",
+                    "emoji": True
+                },
+                "close": {
+                    "type": "plain_text",
+                    "text": "Cancel",
+                    "emoji": True
+                },
+                "private_metadata": str({"channel": body['channel_id'],
+                                     "card": card['id']}),
+                "blocks": [
+                    {
+                        "type": "image",
+                        "title": {
+                            "type": "plain_text",
+                            "text": card['name'],
+                            "emoji": True
+                        },
+                        "image_url": card['image_uris']['small'],
+                        "alt_text": card['name']
+                    },
+                    {
+                        "type": "actions",
+                        "elements": [
+                            {
+                                "type": "button",
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "Shuffle",
+                                    "emoji": True
+                                },
+                                "value": "default",
+                                "action_id": "Random"
+                            }
+                        ]
+                    }
+                ]
+            }
+        )
+
+
+@app.action("post")
+def button_post(ack, body, respond):
+    ack()
+    card_string = body['actions'][0]['value']
+    json_card = ast.literal_eval(card_string)
+    respond(
+        response_type="in_channel",
+        blocks=[json_card],
+        replace_original=True,
+        # as_user=True,
+        delete_original=True
+    )
+
+
+@app.action("delete")
+def button_delete(ack, respond):
+    ack()
+    respond(
+        response_type="ephemeral",
+        text='',
+        replace_original=True,
+        delete_original=True
+    )
+
+
+# Ready? Start your app!
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
---
+++ scry.py
@@ -0,0 +1,92 @@
+import requests
+
+
+def search_card(search_term):
+    url = "https://api.scryfall.com/cards/named?fuzzy="
+    response = requests.get(f"{url}{search_term}")
+    card = response.json()
+    if response.status_code == 200:
+        card_section = {
+                "type": "image",
+                "title": {
+                    "type": "plain_text",
+                    "text": card['name'],
+                    "emoji": True
+                },
+                "image_url": card['image_uris']['normal'],
+                "alt_text": card['name']
+            }
+        blockInfo = [
+            card_section,
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Post",
+                            "emoji": True
+                        },
+                        "value": f"{card_section}",
+                        "style": "primary",
+                        "action_id": "post"
+                    },
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Cancel",
+                            "emoji": True
+                        },
+                        "value": "delete",
+                        "style": "danger",
+                        "action_id": "delete"
+                    }
+                ]
+            }
+        ]
+
+    elif response.status_code == 404:
+        blockInfo = [
+            {
+                "type": "section",
+                "text": {
+                    "type": "plain_text",
+                    "text": card['details']
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Cancel",
+                            "emoji": True
+                        },
+                        "value": "delete",
+                        "style": "danger",
+                        "action_id": "delete"
+                    }
+                ]
+            }
+        ]
+    return blockInfo
+
+
+def random_card():
+    url = "https://api.scryfall.com/cards/random"
+    response = requests.get(url)
+    card = response.json()
+    # print(card)
+    return card
+
+def card_by_id(id):
+    url = "https://api.scryfall.com/cards/"
+    response = requests.get(f"{url}{id}")
+    card = response.json()
+    # print(card)
+    return card
+
