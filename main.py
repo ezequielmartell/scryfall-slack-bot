cc6b916429637ff18810aa4358e825e88ed2d7ac
ezequiel <ezequiel1@thinkmartell.com>
2024-05-17 11:04:27 -0500

    added oauth workflow and reworked actions

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 import os
+from slack_bolt.oauth.oauth_settings import OAuthSettings
+from slack_sdk.oauth.installation_store import FileInstallationStore
+from slack_sdk.oauth.state_store import FileOAuthStateStore
 from slack_bolt import App
 from dotenv import load_dotenv
 import scry
 import ast
+import logging
 
 load_dotenv()
 
-# Initialize your app with your bot token and signing secret
+# logging.basicConfig(level=logging.DEBUG)
+
+user_auth_url = ("https://slack.com/oauth/v2/authorize?state=504c8ee1-f790-41fc-8ac1-b2791af784d3&"
+                 "client_id=7103864971895.7115545845189&user_scope=chat:write")
+user_auth_url = "https://finer-yak-visually.ngrok-free.app/slack/install"
+
+oauth_settings = OAuthSettings(
+    client_id=os.environ["SLACK_CLIENT_ID"],
+    client_secret=os.environ["SLACK_CLIENT_SECRET"],
+    scopes=["commands"],
+    user_scopes=["chat:write"],
+    installation_store=FileInstallationStore(base_dir="./data/installations"),
+    state_store=FileOAuthStateStore(expiration_seconds=600, base_dir="./data/states")
+)
+
 app = App(
-    token=os.environ.get("SLACK_BOT_USER_TOKEN"),
-    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
+    oauth_settings=oauth_settings
 )
 
 
 @app.view("")
-def handle_view_submission_events(ack, body, say):
+def post_random_card(ack, body, say, context):
     ack()
+    user_token = context.get('authorize_result').get('user_token')
     # print(body['view']['blocks'][1]['elements'][0]['value'])
     # card_id = body['view']['blocks'][1]['elements'][0]['value']
     # print(body)
@@ -37,6 +56,7 @@ def handle_view_submission_events(ack, body, say):
     # print(card_section)
     say(
         channel=channel,
+        token=user_token,
         response_type="in_channel",
         as_user=True,
         blocks=[card_section]
@@ -44,14 +64,16 @@ def handle_view_submission_events(ack, body, say):
 
 
 @app.action("Random")
-def handle_some_action(ack, body):
+def get_random_card_modal(ack, body, context):
     ack()
+    user_token = context.get('authorize_result').get('user_token')
     # print(body)
     card = scry.random_card()
     metadata_dict = ast.literal_eval(body['view']['private_metadata'])
     channel = metadata_dict['channel']
     res = app.client.views_update(
         view_id=body["container"]["view_id"],
+        token=user_token,
         view={
             "type": "modal",
             "title": {
@@ -79,7 +101,7 @@ def handle_some_action(ack, body):
                         "text": card['name'],
                         "emoji": True
                     },
-                    "image_url": card['image_uris']['small'],
+                    "image_url": card['image_uris']['normal'],
                     "alt_text": card['name']
                 },
                 {
@@ -103,22 +125,32 @@ def handle_some_action(ack, body):
 
 
 @app.command("/mtgsearch")
-def mtg_search(ack, command, body, say, respond):
+def mtg_search(ack, body, say, context, respond):
+    user_token = context.get('authorize_result').get('user_token')
     # Acknowledge command request
     ack()
-    text = command.get('text')
-    if text:
-        card = scry.search_card(search_term=text)
-        say(
-            response_type="in_channel",
-            blocks=card
-        )
-    else:
-        # print(body)
+    text = body.get('text')
+    if text and user_token:
+        info = scry.search_card(search_term=text)
+        card_block_type = info[0]['type']
+        if card_block_type == "image":
+            say(
+                token=user_token,
+                response_type="in_channel",
+                blocks=info
+            )
+        else:
+            respond(
+                response_type="ephemeral",
+                blocks=info
+            )
+    elif user_token:
+        print(body)
         card = scry.random_card()
         # print(f"random: {card}")
         res = app.client.views_open(
             trigger_id=body["trigger_id"],
+            token=user_token,
             view={
                 "type": "modal",
                 "title": {
@@ -137,7 +169,7 @@ def mtg_search(ack, command, body, say, respond):
                     "emoji": True
                 },
                 "private_metadata": str({"channel": body['channel_id'],
-                                     "card": card['id']}),
+                                         "card": card['id']}),
                 "blocks": [
                     {
                         "type": "image",
@@ -146,7 +178,7 @@ def mtg_search(ack, command, body, say, respond):
                             "text": card['name'],
                             "emoji": True
                         },
-                        "image_url": card['image_uris']['small'],
+                        "image_url": card['image_uris']['normal'],
                         "alt_text": card['name']
                     },
                     {
@@ -166,6 +198,37 @@ def mtg_search(ack, command, body, say, respond):
                     }
                 ]
             }
+        )
+    else:
+        blocks = [
+            {
+                "type": "section",
+                "text": {
+                    "type": "plain_text",
+                    "text": "You'll need to authorize MtGSearch before you can use this bot!"
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Authorize!",
+                            "emoji": True
+                        },
+                        "value": "authorizebutton",
+                        "action_id": "auth_button",
+                        "url": user_auth_url
+                    }
+                ]
+            }
+        ]
+        # print(blocks)
+        respond(
+            response_type="ephemeral",
+            blocks=blocks
         )
 
 
@@ -190,6 +253,17 @@ def button_post(ack, body, say, respond):
 
 @app.action("delete")
 def button_delete(ack, respond):
+    ack()
+    respond(
+        response_type="ephemeral",
+        text='',
+        replace_original=True,
+        delete_original=True
+    )
+
+
+@app.action("auth_button")
+def button_auth(ack, respond):
     ack()
     respond(
         response_type="ephemeral",
